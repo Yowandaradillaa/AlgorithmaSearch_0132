0b15d0ed071d2cc06717b7ae168c44d31b4c6b99
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-04-05 10:13:02 +0700

    Menampilkan enter elemen array

--- PertemuanKetujuhPrakAlgo/PertemuanKetujuhPrakAlgo.cpp
+++ PertemuanKetujuhPrakAlgo/PertemuanKetujuhPrakAlgo.cpp
@@ -16,4 +16,8 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n ";
 	}
+
+	cout << "\n---------------------\n";
+	cout << "Enter Array elements\n";
+	cout << "\n----------------------\n";
 }
